bbc83bfd4ef126c48d0051c47939d89b27175336
Mohalk <Mohalk@10.120.10.185>
2022-05-16 15:41:50 -0300

    CM - pequenas alteracoes

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -3,14 +3,14 @@ package javaapplication1;
 import static controller.UsuarioC.*;
 import java.sql.SQLException;
 /**
- *
+ 
  * @author Mohalk
  */
 public class JavaApplication1 {
 
      public static void main(String[] args) throws SQLException {
         // code application logic here
-        iniciar();
+        iniciar(); // iniciar programa
     }
     
     
